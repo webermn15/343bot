939593ad6c9cf41e4eb647acb30ae867edbde2ba
Michael Weber <webermn15@gmail.com>
2019-01-24 19:41:03 -0600

    cleaned up board sorter

--- utils/boardSorter.js
+++ utils/boardSorter.js
@@ -11,23 +11,3 @@ module.exports = boardSorter = (formattedBoardArray) => {
     if (a.success < b.success) return 1;
 	});
 }
-
-// [ 
-//   { username: 'webs',
-//     emoji: '<:OSFrog:230491442336759809>',
-//     seconds_left: [ 18, 53, 8, 55, 55, 55 ],
-//     success: 6,
-//     true_post: 1 },
-//   { username: 'Puff',
-//     emoji: null,
-//     seconds_left: [ 58, 50, 59, 56, 58, 58 ],
-//     success: 6,
-//     true_post: 0 },
-//   { username: 'Ding',
-//     emoji: '<a:dingif:537067206220054539>',
-//     seconds_left: [ 13, 12, 2, 1, 1, 1, 1, 1, 1, 1 ],
-//     success: 10,
-//     true_post: 0 }
-// ]
-
-// user.seconds_left.length > 0 ? roundResult(user.seconds_left.reduce((x, y) => x + y)/user.seconds_left.length)
