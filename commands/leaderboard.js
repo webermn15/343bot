4fa77ad99b28d9629b0dc3453befad3aff9ad601
Michael Weber <webermn15@gmail.com>
2019-01-24 18:27:11 -0600

    syntax error in board command files

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -9,11 +9,12 @@ module.exports = message => {
 		.then(res => {
 			const formattedLeaderboard = boardMaker(res).sort((x, y) => y.success - x.success);
 			message.channel.send('\`\`\`md\n<Leaderboard < Successful attempts > < Avg secs left >\`\`\`' + 
-				`\n${formattedLeaderboard.map((user, i) => `${user.emoji ? user.emoji : '[' + (i + 1) + ']:'} **${user.username}** ${'-----------------------------------'.slice(user.username.length)} ${user.success} ${'--------------------'} ${user.seconds_left.length > 0 ? roundResult(user.seconds_left.reduce((x, y) => x + y)/user.seconds_left.length) : 'User has not hit any 343'}s \n\n`).join('')}`);})
+				`\n${formattedLeaderboard.map((user, i) => `${user.emoji ? user.emoji : '[' + (i + 1) + ']:'} **${user.username}** ${'-----------------------------------'.slice(user.username.length)} ${user.success} ${'--------------------'} ${user.seconds_left.length > 0 ? roundResult(user.seconds_left.reduce((x, y) => x + y)/user.seconds_left.length) : 'User has not hit any 343'}s \n\n`).join('')}`)
 				.catch(err => {
 					errId = uuidv4();
 					logger.log('error', `${err} , id:${errId}`);
 				});
+		})
 		.catch(err => {
 			errId = uuidv4();
 			logger.log('error', `${err} , id:${errId}`);
