de4cd1f2c5d24f4e7888b77bf81ca1404e5f93e3
Michael Weber <webermn15@gmail.com>
2019-01-24 23:34:35 -0600

    slight improvement tweak to board styling

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -10,7 +10,7 @@ module.exports = message => {
 			const formattedLeaderboard = boardMaker(res);
 			const sortedLeaderboard = boardSorter(formattedLeaderboard);
 			message.channel.send('\`\`\`md\n<Leaderboard < Successful attempts > < Avg secs left >\`\`\`' + 
-			`\n${sortedLeaderboard.map((user, i) => `${user.emoji ? user.emoji : '[' + (i + 1) + ']:'} **${user.username}**\n${'⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀'} **${user.success}** ${'⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀'} **${user.seconds_left.length > 0 ? roundResult(user.seconds_left.reduce((x, y) => x + y)/user.seconds_left.length) : 'User has not hit any 343'}s** \n`).join('')}`)
+			`\n${sortedLeaderboard.map((user, i) => `${user.emoji ? user.emoji : '[' + (i + 1) + ']:'} **${user.username}**\n${'⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀'} **${user.success.toString().length === 1 ? ' ' + user.success + ' ' : user.success}** ${'⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀'} **${user.seconds_left.length > 0 ? roundResult(user.seconds_left.reduce((x, y) => x + y)/user.seconds_left.length) : 'User has not hit any 343'}s** \n`).join('')}`)
 				.catch(err => {
 					errId = uuidv4();
 					logger.log('error', `${err} , id:${errId}`);
